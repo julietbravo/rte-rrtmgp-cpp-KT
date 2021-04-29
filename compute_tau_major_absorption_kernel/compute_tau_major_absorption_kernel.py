63d39cf24d5463305083c00f7357193cbe0b5706
Bart van Stratum <bart@vanstratum.com>
2021-04-29 11:27:34 +0000

    `compute_tau_major_..` working with comparison to output from full gpu code

--- compute_tau_major_absorption_kernel/compute_tau_major_absorption_kernel.py
+++ compute_tau_major_absorption_kernel/compute_tau_major_absorption_kernel.py
@@ -8,15 +8,7 @@ rrtmgp_path = '/home/ubuntu/models/rte-rrtmgp-cpp'      # AWS
 with open('{}/src_kernels_cuda/gas_optics_kernels.cu'.format(rrtmgp_path)) as f:
     kernel_string = f.read()
 
-def print_kernel_string(string):
-    lines = string.split('\n')
-    for l in lines:
-        print(l)
-#print_kernel_string(kernel_string)
-
-#
 # Settings
-#
 type_int = np.int32
 type_float = np.float64
 type_bool = np.int32     # = default without `RTE_RRTMGP_USE_CBOOL`
@@ -33,11 +25,23 @@ neta  = type_int(9)      #
 npres = type_int(59)     # from coefficients_lw.nc (pressure dim)
 ntemp = type_int(14)     # from coefficients_lw.nc (temperature dim)
 
-#
-# Dummy data
-#
+# Output (Array_gpu.dump()) from RCEMIP
+gpoint_flavor = np.fromfile('bin/gpoint_flavor.bin', dtype=type_int)
+band_lims_gpt = np.fromfile('bin/band_lims_gpt.bin', dtype=type_int)
+jeta = np.fromfile('bin/jeta.bin', dtype=type_int)
+jtemp = np.fromfile('bin/jtemp.bin', dtype=type_int)
+jpress = np.fromfile('bin/jpress.bin', dtype=type_int)
+
+kmajor = np.fromfile('bin/kmajor.bin', dtype=type_float)
+col_mix = np.fromfile('bin/col_mix.bin', dtype=type_float)
+fmajor = np.fromfile('bin/fmajor.bin', dtype=type_float)
+
+tropo = np.fromfile('bin/tropo.bin', dtype=type_bool)
+
+# Dummy data -> does not work; kernel computations depend on input data..
+"""
 gpoint_flavor = np.ones(512, dtype=type_int)    # = 2 x ngpt?
-band_lims_gpt = np.ones(32, dtype=type_int)     # = 2 x nband?
+band_lims_gpt = np.ones(32, dtype=type_int)     # = 2 x nband? -> yes, start and end position
 jeta = np.ones(576000, dtype=type_int)          # = ??
 jtemp = np.ones(28800, dtype=type_int)          # = ncol x nlay?
 jpress = np.ones(28800, dtype=type_int)         # = ncol x nlay?
@@ -49,10 +53,16 @@ tau = np.zeros(7372800, dtype=type_float)       # = ?
 tau_major = np.zeros(7372800, dtype=type_float) # = ?
 
 tropo = np.zeros(28800, dtype=type_bool)        # = ncol x nlay?
+"""
 
-#
+# Output from kernel:
+tau = np.zeros(7372800, dtype=type_float)
+tau_major = np.zeros(7372800, dtype=type_float)
+
+# Reference data (from Array_gpu.dump()):
+tau_ref = np.fromfile('bin/tau.bin', dtype=type_float)
+
 # Kernel tuner
-#
 args = [ncol, nlay, nband, ngpt,
         nflav, neta, npres, ntemp,
         gpoint_flavor,
@@ -64,15 +74,33 @@ args = [ncol, nlay, nband, ngpt,
         tau, tau_major]
 
 problem_size = (nband, nlay, ncol)
-
-tune_params = dict()
-tune_params["block_size_x"] = [4,8,16,32]
-tune_params["block_size_y"] = [4,8,16,32]
-tune_params["block_size_z"] = [4,8,16,32]
-
+kernel_name = 'compute_tau_major_absorption_kernel<{}>'.format(str_float)
 cp = ['-I{}/include'.format(rrtmgp_path)]
 
+# Check results
+params = { 'block_size_x': 14,
+           'block_size_y': 1,
+           'block_size_z': 32}
+
+results = kernel_tuner.run_kernel(
+        kernel_name, kernel_string, problem_size,
+        args, params, compiler_options=cp)
+
+results_okay = np.allclose(results[-1], tau_ref, atol=1e-15)
+max_diff = np.abs(results[-1] - tau_ref).max()
+
+print('Results identical={}, max diff={}'.format(results_okay, max_diff))
+
+# Tune!
+tune_params = dict()
+tune_params["block_size_x"] = [4,8,14,16,32,64]
+tune_params["block_size_y"] = [1]
+tune_params["block_size_z"] = [4,8,16,32,64]
+
+answer = len(args)*[None]
+answer[-1] = tau_ref
+
 result, env = kernel_tuner.tune_kernel(
-        f'compute_tau_major_absorption_kernel<{str_float}>',
-        kernel_string, problem_size, args, tune_params,
-        compiler_options=cp)
+        kernel_name, kernel_string, problem_size,
+        args, tune_params, compiler_options=cp,
+        answer=answer, atol=1e-14)
