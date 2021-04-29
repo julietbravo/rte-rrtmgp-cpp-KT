53306d4d3aa3e5d19bfcfa5de7701a786b707f5a
Bart van Stratum <bart@vanstratum.com>
2021-04-29 10:00:43 +0000

    Start indices at one, to prevent out of bounds reads

--- compute_tau_major_absorption_kernel.py
+++ compute_tau_major_absorption_kernel.py
@@ -3,7 +3,8 @@ import numpy as np
 from collections import OrderedDict
 import json
 
-rrtmgp_path = '/home/bart/meteo/models/rte-rrtmgp-cpp'
+#rrtmgp_path = '/home/bart/meteo/models/rte-rrtmgp-cpp'  # Home desktop
+rrtmgp_path = '/home/ubuntu/models/rte-rrtmgp-cpp'      # AWS
 with open('{}/src_kernels_cuda/gas_optics_kernels.cu'.format(rrtmgp_path)) as f:
     kernel_string = f.read()
 
@@ -23,23 +24,23 @@ type_bool = np.int32     # = default without `RTE_RRTMGP_USE_CBOOL`
 str_float = 'float' if type_float is np.float32 else 'double'
 
 ncol  = type_int(144)    # ??, this is RCEMIP..
-nlay  = type_int(200)
-nband = type_int(16)
-ngpt  = type_int(256)
+nlay  = type_int(200)    # case specific
+nband = type_int(16)     # from coefficients_lw.nc (bnd dim)
+ngpt  = type_int(256)    # from coefficients_lw.nc (gpt dim)
 
-nflav = type_int(10)
-neta  = type_int(9)
-npres = type_int(59)
-ntemp = type_int(14)
+nflav = type_int(10)     # ??
+neta  = type_int(9)      # 
+npres = type_int(59)     # from coefficients_lw.nc (pressure dim)
+ntemp = type_int(14)     # from coefficients_lw.nc (temperature dim)
 
 #
 # Dummy data
 #
-gpoint_flavor = np.zeros(512, dtype=type_int)   # = 2 x ngpt?
-band_lims_gpt = np.zeros(32, dtype=type_int)    # = 2 x nband?
-jeta = np.zeros(576000, dtype=type_int)         # = ??
-jtemp = np.zeros(28800, dtype=type_int)         # = ncol x nlay?
-jpress = np.zeros(28800, dtype=type_int)        # = ncol x nlay?
+gpoint_flavor = np.ones(512, dtype=type_int)    # = 2 x ngpt?
+band_lims_gpt = np.ones(32, dtype=type_int)     # = 2 x nband?
+jeta = np.ones(576000, dtype=type_int)          # = ??
+jtemp = np.ones(28800, dtype=type_int)          # = ncol x nlay?
+jpress = np.ones(28800, dtype=type_int)         # = ncol x nlay?
 
 kmajor = np.zeros(1935360, dtype=type_float)    # = ?
 col_mix = np.zeros(576000, dtype=type_float)    # = ?
@@ -65,9 +66,9 @@ args = [ncol, nlay, nband, ngpt,
 problem_size = (nband, nlay, ncol)
 
 tune_params = dict()
-tune_params["block_size_x"] = [4] #[4,8,16,32]
-tune_params["block_size_y"] = [4] #[4,8,16,32]
-tune_params["block_size_z"] = [4] #[4,8,16,32]
+tune_params["block_size_x"] = [4,8,16,32]
+tune_params["block_size_y"] = [4,8,16,32]
+tune_params["block_size_z"] = [4,8,16,32]
 
 cp = ['-I{}/include'.format(rrtmgp_path)]
 
