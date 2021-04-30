ae616913aadcc27fa279f4d1512c6665ac57b0ab
Bart van Stratum <bart@vanstratum.com>
2021-04-30 12:44:51 +0000

    Added script for the split `compute_tau_minor...` kernel

--- compute_tau_absorption_kernel/compute_tau_absorption_kernel_separate.py
+++ compute_tau_absorption_kernel/compute_tau_absorption_kernel_separate.py
@@ -2,8 +2,8 @@ import kernel_tuner
 import numpy as np
 
 # To supress the `int* != BOOL_TYPE` warnings:
-#import warnings
-#warnings.filterwarnings("ignore")
+import warnings
+warnings.filterwarnings("ignore")
 
 #rrtmgp_path = '/home/bart/meteo/models/rte-rrtmgp-cpp'  # Home desktop
 rrtmgp_path = '/home/ubuntu/models/rte-rrtmgp-cpp'      # AWS
@@ -88,30 +88,48 @@ args_major = [
         jtemp, jpress,
         tau, tau_major]
 
-args_minor = [
+itropo_lower = type_bool(1)
+args_minor_lower = [
         ncol, nlay, ngpt,
         ngas, nflav, ntemp, neta,
         nscale_lower,
-        nscale_upper,
         nminor_lower,
-        nminor_upper,
         nminork_lower,
-        nminork_upper,
         idx_h2o,
         gpoint_flavor,
         kminor_lower,
-        kminor_upper,
         minor_limits_gpt_lower,
-        minor_limits_gpt_upper,
         minor_scales_with_density_lower,
-        minor_scales_with_density_upper,
         scale_by_complement_lower,
-        scale_by_complement_upper,
         idx_minor_lower,
-        idx_minor_upper,
         idx_minor_scaling_lower,
-        idx_minor_scaling_upper,
         kminor_start_lower,
+        play,
+        tlay,
+        col_gas,
+        fminor,
+        jeta,
+        jtemp,
+        tropo,
+        itropo_lower,
+        tau,
+        tau_minor]
+
+itropo_upper = type_bool(0)
+args_minor_upper = [
+        ncol, nlay, ngpt,
+        ngas, nflav, ntemp, neta,
+        nscale_upper,
+        nminor_upper,
+        nminork_upper,
+        idx_h2o,
+        gpoint_flavor,
+        kminor_upper,
+        minor_limits_gpt_upper,
+        minor_scales_with_density_upper,
+        scale_by_complement_upper,
+        idx_minor_upper,
+        idx_minor_scaling_upper,
         kminor_start_upper,
         play,
         tlay,
@@ -120,14 +138,16 @@ args_minor = [
         jeta,
         jtemp,
         tropo,
+        itropo_upper,
         tau,
         tau_minor]
+
 
 problem_size_major = (nband, nlay, ncol)
 kernel_name_major = 'compute_tau_major_absorption_kernel<{}>'.format(str_float)
 
 problem_size_minor = (nlay, ncol)
-kernel_name_minor = 'compute_tau_minor_absorption_kernel<{}>'.format(str_float)
+kernel_name_minor = 'compute_tau_minor_absorption_kernel_separate<{}>'.format(str_float)
 
 # Check results
 def compare_fields(arr1, arr2, name):
@@ -153,35 +173,47 @@ compare_fields(result[-2], tau_after_major, 'major')
 params = { 'block_size_x': 32,
            'block_size_y': 32}
 
-# Use output from major as input for major
+# Use output from major as input for minor
 tau[:] = result[-2][:]
 
 result = kernel_tuner.run_kernel(
         kernel_name_minor, kernel_string, problem_size_minor,
-        args_minor, params, compiler_options=cp)
+        args_minor_lower, params, compiler_options=cp)
+
+# Store intermediate results for correctness check in tuning
+tau_after_minor_sub = result[-2][:]
+tau[:] = result[-2][:]
+
+result = kernel_tuner.run_kernel(
+        kernel_name_minor, kernel_string, problem_size_minor,
+        args_minor_upper, params, compiler_options=cp)
 
 compare_fields(result[-2], tau_after_minor, 'minor')
 
+#
 # Tune!
+#
 tune_params_major = dict()
-tune_params_major["block_size_x"] = [4] # [4,8,16,32]
-tune_params_major["block_size_y"] = [4] # [1,2,4]
-tune_params_major["block_size_z"] = [4] # [4,8,16,32]
+tune_params_major["block_size_x"] = [4,8] # [4,8,16,32]
+tune_params_major["block_size_y"] = [4,8] # [1,2,4]
+tune_params_major["block_size_z"] = [4,8] # [4,8,16,32]
 
 tune_params_minor = dict()
-tune_params_minor["block_size_x"] = [4,8,16,24,32,48,64]
-tune_params_minor["block_size_y"] = [4,8,16,24,32,48,64]
+tune_params_minor["block_size_x"] = [4,6,8,10,12,16]
+tune_params_minor["block_size_y"] = [4,6,8,10,12,16]
 
 answer_major = len(args_major)*[None]
 answer_major[-2] = tau_after_major
 
-answer_minor = len(args_minor)*[None]
-answer_minor[-2] = tau_after_minor
+answer_minor_lower = len(args_minor_lower)*[None]
+answer_minor_lower[-2] = tau_after_minor_sub
+
+answer_minor_upper = len(args_minor_upper)*[None]
+answer_minor_upper[-2] = tau_after_minor
 
 # Reset input tau
 tau[:] = 0.
 
-# With result checking:
 result, env = kernel_tuner.tune_kernel(
         kernel_name_major, kernel_string, problem_size_major,
         args_major, tune_params_major, compiler_options=cp,
@@ -191,11 +223,12 @@ tau[:] = tau_after_major
 
 result, env = kernel_tuner.tune_kernel(
         kernel_name_minor, kernel_string, problem_size_minor,
-        args_minor, tune_params_minor, compiler_options=cp,
-        answer=answer_minor, atol=1e-14)
+        args_minor_lower, tune_params_minor, compiler_options=cp,
+        answer=answer_minor_lower, atol=1e-14)
 
+tau[:] = tau_after_minor_sub
 
-# Without result checking (returns timings):
-#result, env = kernel_tuner.tune_kernel(
-#        kernel_name, kernel_string, problem_size,
-#        args, tune_params, compiler_options=cp)
+result, env = kernel_tuner.tune_kernel(
+        kernel_name_minor, kernel_string, problem_size_minor,
+        args_minor_upper, tune_params_minor, compiler_options=cp,
+        answer=answer_minor_upper, atol=1e-14)
